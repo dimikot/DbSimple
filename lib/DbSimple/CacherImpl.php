1ae77b7c1879019ea2cc46398c7d60230021995e
Stepan Legachev <siberex@gmail.com>
2013-10-16 14:26:59 +0900

    improved compatibility

---
+++ lib/DbSimple/CacherImpl.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: sib
+ * Date: 14.10.13
+ * Time: 17:40
+ * To change this template use File | Settings | File Templates.
+ */
+
+class CacherImpl implements Zend_Cache_Backend_Interface {
+
+    protected $callback;
+
+    public function __construct(Callable $callback) {
+        $this->callback = $callback;
+    }
+
+
+    public function clean($mode = Zend_Cache::CLEANING_MODE_ALL, $tags = array()) {}
+
+    public function remove($id) {}
+
+    public function test($id) {}
+
+    public function save($data, $id, $tags = array(), $specificLifetime = false) {
+
+        return call_user_func(array($this, 'callback'), $id, $data);
+    }
+
+    public function load($id, $doNotTestCacheValidity = false) {
+
+        return call_user_func(array($this, 'callback'), $id);
+    }
+
+    public function setDirectives($directives) {}
+
+} // CacherImpl class
